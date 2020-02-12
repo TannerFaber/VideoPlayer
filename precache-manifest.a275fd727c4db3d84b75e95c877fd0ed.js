ce676235436e6b53b18ea2103b86b88b54e27869
Tanner Faber <tannerfaber@Tanners-MacBook-Pro.local>
2020-02-11 20:03:07 -0800

    Updates

---
+++ precache-manifest.a275fd727c4db3d84b75e95c877fd0ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347f817ce57428d7de35a0e75104950b",
+    "url": "/VideoPlayer/index.html"
+  },
+  {
+    "revision": "9b6e54f4534a1e71bbc9",
+    "url": "/VideoPlayer/static/css/main.77330e6a.chunk.css"
+  },
+  {
+    "revision": "3dd18b2f7f05316d551b",
+    "url": "/VideoPlayer/static/js/2.f8584d25.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/VideoPlayer/static/js/2.f8584d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6e54f4534a1e71bbc9",
+    "url": "/VideoPlayer/static/js/main.1c2331ef.chunk.js"
+  },
+  {
+    "revision": "3e45cd733d8745c5687b",
+    "url": "/VideoPlayer/static/js/runtime-main.63a74950.js"
+  }
+]);
